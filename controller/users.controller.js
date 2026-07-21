6941310900a95a2dcf307906b127687ab5791c62
nnspecter <nnspecter@gmail.com>
2026-07-21 12:49:10 +0500

    feat: users controller complete + department routes

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -3,7 +3,7 @@ const db = require('../db');
 class UsersController {
     async createUser(req, res){
         try{
-            const {email, password, role, full_name, position, department_id } = req.body;
+            const { email, password, role, full_name, position, department_id } = req.body;
     
             if(department_id){
                 const findDepartment = await db.query("SELECT * FROM departments WHERE id = $1", [department_id]);
@@ -24,19 +24,71 @@ class UsersController {
     }
 
     async getUsers(req, res){
-
+        try{
+            const allUsers = await db.query("SELECT * FROM users");
+            return res.json(allUsers.rows);
+        }
+        catch(error){
+            return res.status(500).json({ message: "Ошибка сервера", error: error.message });
+        }
     }
 
     async getOneUser(req, res){
-
+        try{
+            const { id } = req.params;
+            const currentUser = await db.query("SELECT * FROM users WHERE id = $1", [id])
+            if(currentUser.rows.length === 0){
+                return res.status(404).json({ message: `Пользователь c id=${id} не найден` });
+            }
+            return res.json(currentUser.rows[0]);
+        }
+        catch(error){
+            return res.status(500).json({ message: "Ошибка сервера", error: error.message });
+        }
     }
 
     async changeUser(req, res){
+        try{
+            const { id } = req.params;
+            const { email, password, role, full_name, position, department_id } = req.body;
+            if(department_id){
+                const findDepartment = await db.query("SELECT * FROM departments WHERE id = $1", [department_id]);
+                if(findDepartment.rows.length === 0){
+                    return res.status(404).json({message: "Департамента не существует"})
+                } 
+            }
 
+            const updatedUser = await db.query(
+                "UPDATE users SET (email, password, role, full_name, position, department_id) = ($2, $3, $4, $5, $6, $7) WHERE id = $1 RETURNING *",
+                [id, email, password, role, full_name, position, department_id]
+            ); 
+            if(updatedUser.rows.length === 0){
+                return res.status(404).json({ message: `Пользователь c id=${id} не найден` });
+            }
+            return res.json(updatedUser.rows[0]);
+        }
+        catch(error){
+            return res.status(500).json({ message: "Ошибка сервера", error: error.message });
+        }
     }
 
     async deleteUser(req, res){
+        try{
+            const { id } = req.params;
+            const deletedUser = await db.query("DELETE FROM users WHERE id=$1 RETURNING *",[id]);
 
+            if(deletedUser.rows.length === 0 ){
+                return res.status(404).json({ message: `Пользователь c id=${id} не найден` });
+            }
+
+            return res.json({ 
+                message: "Пользователь успешно удален", 
+                user: deletedUser.rows[0] 
+            });
+        }
+        catch(error){
+            return res.status(500).json({ message: "Ошибка сервера", error: error.message });
+        }
     }
 
 };
--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -4,5 +4,9 @@ const router = new Router();
 const DepartmentsController = require("../controller/departments.controller")
 
 router.post("/departments", DepartmentsController.createDepartment);
+router.get("/departments", DepartmentsController.getDepartments);
+router.get("/departments/:id", DepartmentsController.getOneDepartment);
+router.put("/departments", DepartmentsController.changeDepartment);
+router.delete("/departments/:id ", DepartmentsController.deleteDepartment);
 
 module.exports = router
